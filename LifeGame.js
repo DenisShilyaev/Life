f24edb263730ea823b8e7775f0c9380d255b492b
unknown <79194954002@yandex.ru>
2019-11-17 13:48:38 +0500

    fifth commit

--- LifeGame.js
+++ LifeGame.js
@@ -1,7 +1,7 @@
 class LifeGame {
     constructor(rows, columns) {
-        this.rows = rows;//Строки
-        this.columns = columns;//Столбцы
+        this.rows = rows; //Строки
+        this.columns = columns; //Столбцы
 
         this.map = []; //Создаем матрицу
 
@@ -14,41 +14,50 @@ class LifeGame {
 
             this.map.push(row);
         }
+    }
 
-        reviveRandomFields(n = 1) {
-            const freeFields = []; //Создаем новый массив для пустых (свободных) ячеек
+    reviveRandomFields(n = 1) {
+        const freeFields = []; //Создаем новый массив для пустых (свободных) ячеек
 
-            for (let y = 0; y < this.rows; y++) { //Заполняем данный массив 
-                for (let x = 0; x < this.columns; x++) {
-                    if (this.getField(x, y) === false) {
-                        freeFields.push({ x, y });
-                    }
+        for (let y = 0; y < this.rows; y++) { //Заполняем данный массив 
+            for (let x = 0; x < this.columns; x++) {
+                if (this.getField(x, y) === false) {
+                    freeFields.push({ x, y });
                 }
-            }
-
-            n = parseInt(n);//Проверяем n
-            n = Math.min(n, freeFields.length);//Проверяем, что бы n не выходила за пределы массива
-
-            while (n > 0) {
-                const index = Math.floor(Math.random() * freeFields.length)//Записываем координаты случайной ячейки поля
-                const { x, y } = freeFields.splice(index, 1)[0]//"Изымаем"" данную ячейку из поля
-                this.setField(x, y, true) //Записываем в данную ячейку true
             }
         }
 
+        n = parseInt(n); //Проверяем n
+        n = Math.min(n, freeFields.length); //Проверяем, что бы n не выходила за пределы массива
 
-        getField(x, y) {//Геттерн
-            if (x < 0 || x >= this.columns || y < 0 || y >= this.rows) {
-                return false
-            }
-            return this.map[y][x];
-        };
+        while (n-- > 0) {
+            const index = Math.floor(Math.random() * freeFields.length) //Записываем координаты случайной ячейки поля
+            const { x, y } = freeFields.splice(index, 1)[0] //"Изымаем"" данную ячейку из поля
+            this.setField(x, y, true) //Записываем в данную ячейку true
+        }
+    };
 
-        setField(x, y, value) {//Сеттерн
-            if (x < 0 || x >= this.columns || y < 0 || y >= this.rows) {
-                return value
+    forFreeEach(hander) {
+        for (let y = 0; y < this.rows; y++) {//Перебираем всё поле
+            for (let x = 0; x < this.columns; x++) {
+                if (this.getField(x, y) === true) { //Если ячейка содержит true (то есть она живая)
+                    hander(x, y); //Вызываем hander с данными координатами
+                }
             }
-            return this.map[y][x] = value;
-        };
+        }
     }
+
+    getField(x, y) { //Геттерн
+        if (x < 0 || x >= this.columns || y < 0 || y >= this.rows) {
+            return false
+        }
+        return this.map[y][x];
+    };
+
+    setField(x, y, value) { //Сеттерн
+        if (x < 0 || x >= this.columns || y < 0 || y >= this.rows) {
+            return value
+        }
+        return this.map[y][x] = value;
+    };
 }
