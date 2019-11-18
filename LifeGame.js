c00fecac931444300e9441bf7c5cf5c9628a59fd
unknown <79194954002@yandex.ru>
2019-11-18 21:50:31 +0500

    7 commit

--- LifeGame.js
+++ LifeGame.js
@@ -2,6 +2,7 @@ class LifeGame {
     constructor(rows, columns) {
         this.rows = rows; //Строки
         this.columns = columns; //Столбцы
+        this.generationNumber = 0; //Количество поколений, которое уже прошло
 
         this.map = []; //Создаем матрицу
 
@@ -14,6 +15,46 @@ class LifeGame {
 
             this.map.push(row);
         }
+    }
+
+    changeGeneration() { //Генератор жизни. Создаем новое поле со следующей итерацией жизни.
+        const map = []
+
+        for (let y = 0; y < this.rows; y++) {
+            const row = []
+
+            for (let x = 0; x < this.columns; x++) {
+                let neighborsNumber = 0;
+                let state = false; //Состояние клетки (true - живая, false - мёртвая)
+
+                for (let dx = -1; dx <= 1; dx++) { //Ниже пробегаемся по соседним клеткам и считаем количество живых соседей
+                    for (let dy = -1; dy <= 1; dy++) {
+                        if (dx === 0 && dy === 0) { //Не учитываем саму летку. То сть сама клетка не может являтся себе соседом.
+                            continue
+                        }
+
+                        neighborsNumber += this.getField(x + dx, y + dy) //Записываем количество живых соседей
+                    }
+                }
+
+                if (this.getField(x, y) === true) { //Жизнь сохраняется если рядом находятся 2 или 3 живые клетки
+                    if (neighborsNumber === 2 || neighborsNumber === 3) {
+                        state = true
+                    }
+                } else {
+                    if (neighborsNumber === 3) { //Жизнь появляется в новой клетке если рядом находятся 3 живых соседа
+                        state = true
+                    }
+                }
+
+                row.push(state) //"Оживляем" или "убиваем" клетку
+            }
+
+            map.push(row)
+        }
+
+        this.map = map
+        this.generationNumber++
     }
 
     reviveRandomFields(n = 1) {
@@ -38,7 +79,7 @@ class LifeGame {
     };
 
     forFreeEach(hander) {
-        for (let y = 0; y < this.rows; y++) {//Перебираем всё игровое поле
+        for (let y = 0; y < this.rows; y++) { //Перебираем всё игровое поле
             for (let x = 0; x < this.columns; x++) {
                 if (this.getField(x, y) === true) { //Если ячейка содержит true (то есть она живая)
                     hander(x, y); //Вызываем hander с данными координатами
--- game.js
+++ game.js
@@ -3,12 +3,13 @@ const ROWS_NUMBER = 10; //Количество строк
 const COLUMNS_NUMBER = 10; //Количество столбцов
 const BACKGROUND_COLOR = 'gray'; //Цвет фона
 const FIELD_COLOR = 'blue'; //Цвет жизни
+const GENERATION_TIME = 1000; //Время, через которое будет производится итерация обновления жизни
 
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-const lifeGame = new LifeGame (ROWS_NUMBER, COLUMNS_NUMBER);
+const lifeGame = new LifeGame(ROWS_NUMBER, COLUMNS_NUMBER);
 
 const start = () => {
     canvas.width = FIELD_SIZE * COLUMNS_NUMBER //Задаем ширину canvas
@@ -19,21 +20,26 @@ const start = () => {
     requestAnimationFrame(tick());
 }
 
-const tick = () => { //Функция обновляет экран
+const tick = (timestamp) => { //Функция обновляет экран
     clearCanvas(); //Очищаем поле
-    lifeGame.forFreeEach((x, y) => drawField (x, y, FIELD_COLOR));//Отрисовываем живые клетки
 
-	requestAnimationFrame(tick());//Зацикливаем функцию
+    if (timestamp > lifeGame.generationNumber * GENERATION_TIME) {//Сменяем поколение через определенное время
+        lifeGame.changeGeneration();
+    }
+
+    lifeGame.forFreeEach((x, y) => drawField(x, y, FIELD_COLOR)); //Отрисовываем живые клетки
+
+    requestAnimationFrame(tick()); //Зацикливаем функцию
 }
 
-const clearCanvas = () => {//Очищаем поле
+const clearCanvas = () => { //Очищаем поле
     context.fillStyle = BACKGROUND_COLOR; //Задаем цвет
-    context.beginPath();  
+    context.beginPath();
     context.rect(0, 0, canvas.width, canvas.height); //Отрисовываем квадрат
     context.fill();
 }
 
-const drawField = (x, y, color) => {//Отрисовываем ячейку
+const drawField = (x, y, color) => { //Отрисовываем ячейку
     context.fillStyle = color
     context.beginPath()
     context.rect(x * FIELD_SIZE, y * FIELD_SIZE, FIELD_SIZE, FIELD_SIZE)
@@ -41,4 +47,3 @@ const drawField = (x, y, color) => {//Отрисовываем ячейку
 }
 
 start();
-
